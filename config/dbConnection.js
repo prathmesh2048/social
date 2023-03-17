11b3d8c55bceced8107bfa063ceb79e5a51d605f
prathmesh <prathmeshnandurkar123@gmail.com>
2023-03-17 22:46:48 +0530

    changes db

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 let pool = '';
 try {
   pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'social_media',
-    password: 'post123',
-    port: '5432',
+    user: 'prathmesh',
+    host: 'dpg-cga9qt02qv25khlhciv0-a.oregon-postgres.render.com',
+    database: 'social_media_ieiu',
+    password: 'ibF06agXAAUodeipawuK4B88XKocaYGX',
+    port: 5432, // Port should be a number, not a string,
   });
 } catch (err) {
   console.log(err);
@@ -15,3 +15,5 @@ try {
 
 
 module.exports = pool;
+// postgres://prathmesh:ibF06agXAAUodeipawuK4B88XKocaYGX@dpg-cga9qt02qv25khlhciv0-a/social_media_ieiu
+// PGPASSWORD=ibF06agXAAUodeipawuK4B88XKocaYGX psql -h dpg-cga9qt02qv25khlhciv0-a.oregon-postgres.render.com -U prathmesh social_media_ieiu
