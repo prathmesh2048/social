2b411f8fcf3fa76ece8e42cded1095df681f67b6
prathmesh <prathmeshnandurkar123@gmail.com>
2023-03-17 23:07:03 +0530

    adjustments

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,11 +2,15 @@ const { Pool } = require('pg');
 let pool = '';
 try {
   pool = new Pool({
-    user: 'prathmesh',
-    host: 'dpg-cga9qt02qv25khlhciv0-a.oregon-postgres.render.com',
-    database: 'social_media_ieiu',
-    password: 'ibF06agXAAUodeipawuK4B88XKocaYGX',
-    port: 5432, // Port should be a number, not a string,
+    // user: 'prathmesh',
+    // host: 'dpg-cga9qt02qv25khlhciv0-a.oregon-postgres.render.com',
+    // database: 'social_media_ieiu',
+    // password: 'ibF06agXAAUodeipawuK4B88XKocaYGX',
+    // port: 5432, // Port should be a number, not a string,
+    connectionString: 'postgres://prathmesh:ibF06agXAAUodeipawuK4B88XKocaYGX@dpg-cga9qt02qv25khlhciv0-a.oregon-postgres.render.com/social_media_ieiu',
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
 } catch (err) {
   console.log(err);
